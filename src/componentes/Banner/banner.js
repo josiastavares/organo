1e17c158ecd9028287f0b9a9b62c2673db18dc74
josiastavares <eduardosantostavares13@gmail.com>
2023-11-15 23:22:57 -0300

    deploy organo

---
+++ src/componentes/Banner/banner.js
@@ -0,0 +1,9 @@
+import './banner.css'
+
+export const Banner = () => {
+    return(
+        <header className="banner">
+            <img src="/imagens/banner.png" alt="O banner principal da página do Organo"/>
+        </header>
+    )
+}
